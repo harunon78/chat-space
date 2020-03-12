02bf961dbbef94f21b6b85c4a4a8c8ad53e8cfc5
harunon78 <noon.spy-luv95@softbank.ne.jp>
2020-03-12 15:36:18 +0900

    変更しました

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,8 @@
 class GroupsController < ApplicationController
 
+
   def index
+    
   end
 
   def new
@@ -15,24 +17,24 @@ class GroupsController < ApplicationController
     else
       render :new
     end
-
-    def edit
-      @group = Group.find(params[:id])
-    end
-  
-    def update
-      @group = Group.find(params[:id])
-      if @group.update(group_params)
-        redirect_to root_path, notice: 'グループを更新しました'
-      else
-        render :edit
-      end
-    end
-  
-
   end
-    private
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
   end
+
 end
