6bbfed880d66b9f2583756328ae6355744704501
harunon78 <noon.spy-luv95@softbank.ne.jp>
2020-03-19 20:26:03 +0900

    変更しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -36,51 +36,52 @@ $(function(){
     };
   }
 $('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $('form')[0].reset();
+        })
+      .fail(function(){
+        alert('メッセージを入力してください。');
+        });
+        
+        return false;
+        })
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
     })
-    .fail(function(){
+    .fail(function() {
       alert('error');
     });
-      
-      return false;
-    })
-var reloadMessages = function() {
-  var last_message_id = $('.message:last').data("message-id");
-  $.ajax({
-    url: "api/messages",
-    type: 'get',
-    dataType: 'json',
-    data: {id: last_message_id}
-  })
-  .done(function(messages) {
-    if (messages.length !== 0) {
-      var insertHTML = '';
-      $.each(messages, function(i, message) {
-        insertHTML += buildHTML(message)
-      });
-      $('.messages').append(insertHTML);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
     }
-  })
-  .fail(function() {
-    alert('error');
-  });
-  };
-if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-  setInterval(reloadMessages, 7000);
-  }
 });
